7d20168bd7deb03cf713ab437f06d8e8f7528775
Axedro <axedro@yahoo.es>
2013-12-07 18:26:10 +0000

    Build comprimido que funciona

--- espanol/build/cocos2d.js
+++ espanol/build/cocos2d.js
@@ -33,16 +33,12 @@
         showFPS:false,
         loadExtension:true,
         frameRate:60,
-        renderMode:1,       //Choose of RenderMode: 0(default), 1(Canvas only), 2(WebGL only)
+        renderMode:0,       //Choose of RenderMode: 0(default), 1(Canvas only), 2(WebGL only)
         tag:'gameCanvas', //the dom element to run cocos2d on
         //engineDir:'http://wdbasic.com/cocos2d/cocos2d/',
-        SingleEngineFile:'http://wdbasic.com/cocos2d/lib/Cocos2d-html5-v2.2.1.min.js',
+        SingleEngineFile:'Memorion.min.js',
         appFiles:[
-            'src/resource.js',
-            'src/myApp.js',//add your own files in order here
-            'src/Juego.js',
-            'src/Instrucciones.js',
-            'src/CCCustomLoader.js'
+            
         ]
     };
 
